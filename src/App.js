15709bbc86d459425054c1014eb04fe9ca4c3721
georgedzi <georgedziouba@gmail.com>
2023-02-13 15:54:43 -0500

    fix some lint errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import StarMatch from 'StarMatch/StarMatch';
+import React from "react";
+import StarMatch from "StarMatch/StarMatch";
 
 function App() {
   return (
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+import { test, expect } from "@jest/globals";
+import "@testing-library/jest-dom/extend-expect"; // Import the jest-dom library
 
-test('renders learn react link', () => {
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
